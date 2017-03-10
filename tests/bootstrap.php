b857d63bf33bfad2c8427289e72658096f8a5568
Linus Metzler <linus.metzler@gmail.com>
2017-03-10 18:47:38 +0100

    renamed payloads, added Travis tag payload

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,5 +3,6 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 define('BASEPATH', realpath(__DIR__.'/../'));
-define('DATA_WEBHOOK_GITHUB', realpath(__DIR__.'/data/webhook-github.json'));
-define('DATA_WEBHOOK_TRAVIS', realpath(__DIR__.'/data/webhook-travis.json'));
+define('DATA_WEBHOOK_GITHUB', realpath(__DIR__.'/data/webhook_github.json'));
+define('DATA_WEBHOOK_TRAVIS_BRANCH', realpath(__DIR__.'/data/webhook_travis-branch.json'));
+define('DATA_WEBHOOK_TRAVIS_TAG', realpath(__DIR__.'/data/webhook_travis-tag.json'));
