ca719b363517f0f20cca3d868dee70e0c3a862d6
Linus Metzler <linus.metzler@gmail.com>
2017-03-10 10:27:28 +0100

    add example Travis webhook

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,3 +4,4 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 define('BASEPATH', realpath(__DIR__.'/../'));
 define('DATA_WEBHOOK_GITHUB', realpath(__DIR__.'/data/webhook-github.json'));
+define('DATA_WEBHOOK_TRAVIS', realpath(__DIR__.'/data/webhook-travis.json'));
